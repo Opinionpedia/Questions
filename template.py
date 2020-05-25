102508b2d10d9a04e24218feba9946b317a1808f
Alexander McNulty <Alexander.mcnulty92@gmail.com>
2020-05-25 09:19:45 -0700

    created json smp500

---
+++ template.py
@@ -0,0 +1,13 @@
+startString = "{Title:'Is "
+endString = " a good company?', Tags: ['business', 'america', 'finance'], Answers: ['yes', 'no', 'not sure']}"
+
+f = open("smp500.txt", "r")
+
+w = open("smp500.json", "w")
+
+w.write("Questions: [")
+
+for x in f:
+    w.write(startString  + f.readline()[:-1] + endString + ",")
+
+w.close()
