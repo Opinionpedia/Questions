3e8a9d0aa4a336c34795d35d6efa933fbe812392
Alexander McNulty <Alexander.mcnulty92@gmail.com>
2020-05-25 09:35:28 -0700

    correct json format

--- template.py
+++ template.py
@@ -1,13 +1,14 @@
-startString = "{Title:'Is "
-endString = " a good company?', Tags: ['business', 'america', 'finance'], Answers: ['yes', 'no', 'not sure']}"
+startString = '{"Title":"Is '
+endString = ' a good company?", "Tags": ["business", "america", "finance"], "Answers": ["yes", "no", "not sure"]}'
 
 f = open("smp500.txt", "r")
 
 w = open("smp500.json", "w")
 
-w.write("Questions: [")
+w.write("[")
 
 for x in f:
     w.write(startString  + f.readline()[:-1] + endString + ",")
 
+w.write("]")
 w.close()
